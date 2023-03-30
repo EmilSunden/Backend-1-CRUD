533d2d56704fa3695fed0246a6ffdcab3d708a3e
EmilSunden <emilsunjobb@gmail.com>
2023-03-30 19:29:37 +0200

    added validation for getTodo

--- backend/src/controllers/todo/getTodo.js
+++ backend/src/controllers/todo/getTodo.js
@@ -1,7 +1,13 @@
 const { pool } = require('../../config/database');
+const { getTodoSchema } = require('../../Model/GetTodo');
 
 const getTodoController = (req, res) => {
     const { id } = req.params
+
+    const validation = getTodoSchema.validate(req.params);
+    if (validation.error) {
+        return res.status(400).json(validation.error.details[0].message);
+    }
 
     const getTodos = `SELECT description from todos where id = ?`;
 
